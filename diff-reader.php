b519114f57cada32b8a09067ba51eb820c28edd5
Sergey boonya Buynitskiy <boonya41@gmail.com>
2013-12-16 16:21:18 +0200

    Convert indentation to spaces

--- diff-reader.php
+++ diff-reader.php
@@ -11,24 +11,23 @@ $reader->getFile($file);
         <title>Diff Reader: <?php echo $file ?></title>
         <link media="all" rel="stylesheet" type="text/css" href="/diff-reader/main.css" />
         <style>
-			.file-info {color: #FFFFFF;}
-			.content-start {color: #00CCCC;}
-			.added {color: #00AA00;}
-			.removed {color: #AA0000;}
-			.empty-string {background-color: #AA0000;}
+            .file-info {color: #FFFFFF;}
+            .content-start {color: #00CCCC;}
+            .added {color: #00AA00;}
+            .removed {color: #AA0000;}
+            .empty-string {background-color: #AA0000;}
         </style>
     </head>
-	<body>
-		<?php $content = $reader->getContent();
-			if (empty($content)): ?>
-			<div class="file-not-found">File not found</div>
-		<?php else: ?>
-			<ol>
-			<?php foreach ($content as $i => $line) : ?>
-				<li<?php echo (!empty($line['class'])) ? ' class="' . $line['class'] . '"' : '' ?>><?php echo $line['content'] ?></li>
-			<?php endforeach; ?>
-			</ol>
-		<?php endif; ?>
-		<!--<div class="command-input">[<?php echo $username ?>@dating-<?php echo $location ?> <?php echo $currentDir ?>]$&nbsp;</div>-->
-	</body>
+    <body>
+        <?php $content = $reader->getContent();
+            if (empty($content)): ?>
+            <div class="file-not-found">File not found</div>
+        <?php else: ?>
+            <ol>
+            <?php foreach ($content as $i => $line) : ?>
+                <li<?php echo (!empty($line['class'])) ? ' class="' . $line['class'] . '"' : '' ?>><?php echo $line['content'] ?></li>
+            <?php endforeach; ?>
+            </ol>
+        <?php endif; ?>
+    </body>
 </html>
--- ls.php
+++ ls.php
@@ -7,30 +7,30 @@ $reader = new DiffReader();
     <head>
         <meta charset="utf-8" />
         <title>Diff Reader: <?php echo $file ?></title>
-		<link media="all" rel="stylesheet" type="text/css" href="/diff-reader/main.css" />
+        <link media="all" rel="stylesheet" type="text/css" href="/diff-reader/main.css" />
         <style>
-        	ol li a {
-        		color: #999999;
-        		text-decoration: none;
-        		border-left: transparent 2px solid;
-        		padding-left: 5px;
-        	}
-        	ol li a:hover {
-        		border-left-color: #00AA00;
-        	}
+            ol li a {
+                color: #999999;
+                text-decoration: none;
+                border-left: transparent 2px solid;
+                padding-left: 5px;
+            }
+            ol li a:hover {
+                border-left-color: #00AA00;
+            }
         </style>
     </head>
-	<body>
-		<?php $listing = $reader->listing(__DIR__);
-			if (empty($listing)): ?>
-			<div class="file-not-found">Files not found</div>
-		<?php else: ?>
-			<ol>
-			<?php foreach ($listing as $i => $item) : ?>
-				<li><a href="./<?php echo $item ?>"><?php echo $item ?></a></li>
-			<?php endforeach; ?>
-			</ol>
-		<?php endif; ?>
-		<!--<div class="command-input">[<?php echo $username ?>@dating-<?php echo $location ?> <?php echo $currentDir ?>]$&nbsp;</div>-->
-	</body>
+    <body>
+        <?php $listing = $reader->listing(__DIR__);
+            if (empty($listing)): ?>
+            <div class="file-not-found">Files not found</div>
+        <?php else: ?>
+            <ol>
+            <?php foreach ($listing as $i => $item) : ?>
+                <li><a href="./<?php echo $item ?>"><?php echo $item ?></a></li>
+            <?php endforeach; ?>
+            </ol>
+        <?php endif; ?>
+        <!--<div class="command-input">[<?php echo $username ?>@dating-<?php echo $location ?> <?php echo $currentDir ?>]$&nbsp;</div>-->
+    </body>
 </html>
